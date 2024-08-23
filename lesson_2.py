8bbe4593dcd254e0ddf3126bae5b2216f854b598
Koazt97 <ipavlov.kk@gmail.com>
2024-08-24 01:09:34 +0300

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,5 @@
+amount_of_homework = a = 12
+amount_of_hours = b = 1.5
+name_of_course = c = 'Python'
+time_spent_on_1_homework = d = 0.125
+print('Курс:', c[0:],',', 'всего задач:', a,',', 'затрачено часов:', b,',', 'среднее время выполнения:', d,'.')
